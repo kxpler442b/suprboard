ed0d74fa4a73a1572946533a51f3e592bf69e424
kxpler442b <benmoss2002@fastmail.co.uk>
2023-05-26 05:51:29 +0100

    feat: basic session middleware

--- app/middleware.php
+++ app/middleware.php
@@ -3,8 +3,9 @@
 declare(strict_types = 1);
 
 use Slim\App;
+use App\Http\Middleware\SessionMiddleware;
 
 return function(App $app)
 {
-    
+    $app->add(SessionMiddleware::class);
 };
---
+++ src/Http/Middleware/SessionMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Http\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class SessionMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        session_start();
+        
+        $request = $request->withAttribute('session', $_SESSION);
+
+        return $handler->handle($request);
+    }
+}
